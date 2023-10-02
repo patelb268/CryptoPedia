24d9d295f332fb7c9354b28a11842fc80f488254
PRAYAG PATEL <patel.prayag.b@gmail.com>
2023-10-02 12:11:59 -0700

    created basic layout for the main content

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import App from "./App";
 
+import App from "./App";
+import 'antd/dist/reset.css';
 ReactDOM.render(
     <Router>
         <App />
