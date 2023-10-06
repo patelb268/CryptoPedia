923862840f9f02f1af4a03643858119addca2a85
PRAYAG PATEL <patel.prayag.b@gmail.com>
2023-10-05 23:23:40 -0700

    created collapsible navigation bar

--- src/App.js
+++ src/App.js
@@ -28,18 +28,6 @@ const App = () => {
                     </Routes>
                 </div>
             </Layout>
-        
-
-            <div className='footer'>
-                <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
-                    CryptoPedia <br />
-                    All rights reserved
-                </Typography.Title>
-                <Space>
-                    <Link to='/'>Home</Link>
-                    <Link to='/news'>News</Link>
-            </Space>
-            </div>
         </div>
         
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Menu, Typography, Avatar } from 'antd';
+import React, { useState } from 'react';
+import { Menu, Typography, Avatar, Layout } from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
-
+import { HomeOutlined, BulbOutlined, FundOutlined } from '@ant-design/icons';
 import icon from '../assets/icon.png';
 
+const { Sider } = Layout;
+
 const Navbar = () => {
-  
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <div className="nav-container">
-      <div className="logo-container">
-        <Avatar src={icon} size='large'/>
-        <Typography.Title level={2} className="logo"><Link to="/">CryptoPedia</Link></Typography.Title>
-        
-      </div>
-      <Menu className='menu'>
-        <Menu.Item icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item icon={<FundOutlined />}>
-          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-        </Menu.Item>
-        <Menu.Item icon={<BulbOutlined />}>
-          <Link to="/news">News</Link>
-        </Menu.Item>
-      </Menu>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+        className='sider nav-container'
+      >
+        {/* Logo and Title outside the collapsed menu */}
+        <div className='logo-container'>
+          <Avatar src={icon} size='large' />
+          {!collapsed && (<Typography.Title level={2} className='logo'>
+            <Link className='title' to="/">CryptoPedia</Link>
+          </Typography.Title>)}
+        </div>
+
+        {/* Menu Items */}
+        <Menu className='menu' theme='dark' mode='inline'>
+          <Menu.Item key='home' icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+
+          <Menu.Item key='cryptocurrencies' icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+
+          <Menu.Item key='news' icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+    </Layout>
   );
 };
 
